e68e04b99bdcfcb69f277a0e2beec4d379cfedb9
Raakas <arttu.tiiro@gmail.com>
2019-06-12 12:21:18 +0300

    Display API data

--- testing/src/App.js
+++ testing/src/App.js
@@ -2,18 +2,34 @@ import React from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import actionOne from './actions/basicActions';
-import getWeather from './openWeatherMapAPI';
 import WeatherComponent from './components/WeatherComponent';
+import axios from 'axios';
+
+const API = '037362528e461435f3315b2e8feadf50';
+const URL = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+const city = 'Oulu';
 
 class App extends React.Component {
 
     state = {
-      data: 'cityNameHere'
+      city: 'cityNameHere'
     }
 
+    getWeather() {
+      axios.get(`${URL}${city}&appid=${API}`)
+        .then(data => {
+
+          const city = JSON.stringify(data.data.city.name);
+          
+            this.setState({
+              city: city
+            })
+          })
+          .catch(error => console.log(error));
+    } 
+
     componentDidMount(){
-      const city = getWeather();
-      console.log(city);
+      this.getWeather();
     }
 
   render () {
@@ -32,7 +48,7 @@ class App extends React.Component {
         <Route path="/forecast" render={() => (
           <div>
           <h3>Forecast</h3>
-          <p>{this.state.data}</p>
+          <p>{this.state.city}</p>
           <WeatherComponent 
             changeState={this.changeState.bind(this)}
           />
